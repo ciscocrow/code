d8f8a9f9d3b62d0ee5cf6d4204cd7c32c8a5c851
ciscocrow <dacrow@cisco.com>
2022-02-11 13:47:35 -0500

    Excercise 7

---
+++ Python_The_Hard_Way/ex5.py
@@ -0,0 +1,14 @@
+name = 'David B Crow'
+age = 43 #not a lie
+height = 70 * 2.54 #1 inch = 2.54 CM
+weight = round(180 * 0.453592) #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"My Height in CM is {height}.")
+print(f"My Weight in KG is {weight}.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}")
---
+++ Python_The_Hard_Way/ex7.py
@@ -0,0 +1,11 @@
+print("Mary had a little lamb.")
+print("its fleece was white as {}.".format('snow'))
+print("and everywhere that mary went.")
+print("." * 10) # printing 10 .
+
+end1 = "C"
+end2 = "h"
+end3 = "eese"
+end5 = "Burger"
+print(end1 + end2 + end3, end=' ')
+print(end5)
