ee4359f6f7a69e6192dac6e12b85eac301e8d8c3
ciscocrow <dacrow@cisco.com>
2022-02-11 14:37:35 -0500

    Stage 11

---
+++ Python_The_Hard_Way/ex11.py
@@ -0,0 +1,8 @@
+#print('how old are you?', end=' ')
+age = 2 * int(input("HOw old are you? "))
+#print("how tall are you?", end=' ')
+height = input("How tall are you? ")
+#print("how much do you weigh?", end=' ')
+weight = input("how much do you weigh? ")
+
+print(f"so you're {age} years old, {height} tall, and weigh {weight} lbs.")
---
+++ Python_The_Hard_Way/ex9.py
@@ -0,0 +1,13 @@
+# Her's some strange stuff
+
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months ="\nJan\nFeb\nMar\nApr\nMay\nJun"
+
+print("here are the days: ", days)
+print(f"here are the months: {months}")
+
+print("""
+This one is fun you can type whatever you want here.
+Keep going and going
+Forever..
+""")
